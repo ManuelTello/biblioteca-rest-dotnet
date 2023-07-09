ebdfc4407c861d4b6f88ab075b6fe74207dcfb4b
Manuel Tello Siri <manuelTello.business@gmail.com>
2023-07-09 04:51:17 -0300

    Cambios en el DTO de Book

--- biblioteca-dotnet/Dto/BookDTO.cs
+++ biblioteca-dotnet/Dto/BookDTO.cs
@@ -2,11 +2,15 @@
 {
     public class BookDTO
     {
+        public int Id { get; set; }
+
         public string? Title { get; set; }
 
         public string? DateWritten { get; set; }
 
         public string? Publisher { get; set; }
+
+        public int Rented { get; set; }
 
         public List<string> Authors { get; set; }
 
