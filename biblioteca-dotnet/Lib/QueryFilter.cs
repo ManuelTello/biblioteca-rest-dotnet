5f9380d2035d69eec51c57b6e5c740d7a77e3132
Manuel Tello Siri <manuelTello.business@gmail.com>
2023-07-09 04:51:45 -0300

    Nueva clase para filtrar querys

---
+++ biblioteca-dotnet/Lib/QueryFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace biblioteca_dotnet.Lib
+{
+    public class QueryFilter
+    {
+        public static string CleanString(string query)
+        {
+            Regex non_alpha_numeric = new Regex("[^a-zA-Z0-9#\\s]", RegexOptions.IgnoreCase);
+            string query_trimmed = non_alpha_numeric.Replace(query, "");
+            return query_trimmed;
+        }
+
+        public static List<string> CleanAndStripAlphaNumeric(string query)
+        {
+            string query_trimmed = QueryFilter.CleanString(query);
+            List<string> query_filtered = query_trimmed.Split("#").ToList();
+            return query_filtered;
+        }
+
+
+    }
+}
